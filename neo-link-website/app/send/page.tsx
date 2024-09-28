451383947f1e3bb7ef459722cea43c08e366de3d
dipanshuhappy <dipanshuhappy@gmail.com>
2024-09-28 23:16:05 +0530

    feat: Add additional parameters to SendPage component
    This commit adds additional parameters to the SendPage component in the Neo Link website. The parameters include neoLinkAddress, useWriteNeoLinkWithdrawDeposit, generateKeyFromString, getRandomString, signWithdrawalMessage, useAccount, and useChainId. It also includes the functions claimDeposit and makeDeposit, which handle the claiming and making of deposits respectively. The commit also updates the makeDeposit function to include a value of 10000000000000. Additionally, the commit adds new constants NEOLINK_SALT, ANYONE_WITHDRAWAL_MODE, and RECIPIENT_WITHDRAWAL_MODE to the constants file.

--- neo-link-website/app/send/page.tsx
+++ neo-link-website/app/send/page.tsx
@@ -1,20 +1,59 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { NULL_ADDRESS } from "@/lib/constants";
-import { useWriteNeoLinkMakeCustomDeposit } from "@/lib/smart-contract";
-import { generateKeyFromString, getRandomString } from "@/lib/utils";
-import { useAccount } from "wagmi";
-import Send from '@/components/Send'
+
+import Send from "@/components/Send";
+import {
+  neoLinkAddress,
+  useWriteNeoLinkMakeCustomDeposit,
+  useWriteNeoLinkWithdrawDeposit,
+} from "@/lib/smart-contract";
+import {
+  generateKeyFromString,
+  getRandomString,
+  signWithdrawalMessage,
+} from "@/lib/utils";
+import { useAccount, useChainId } from "wagmi";
 
 export default function SendPage() {
   const { writeContractAsync } = useWriteNeoLinkMakeCustomDeposit();
+  const { writeContractAsync: claim } = useWriteNeoLinkWithdrawDeposit();
+  const chainId = useChainId();
   const { address } = useAccount();
-  const makeDesposit = async () => {
+
+  const claimDeposit = async () => {
+    if (!address) {
+      alert("Please connect your wallet");
+      return;
+    }
+    const vaultAddress = neoLinkAddress[chainId as keyof typeof neoLinkAddress];
+    if (!vaultAddress) {
+      alert("Vault address not found");
+      return;
+    }
+
+    const { depositIdx, recipient, signature } = await signWithdrawalMessage(
+      "1",
+      chainId.toString(),
+      vaultAddress,
+      0,
+      address,
+      generateKeyFromString("psxY_zyKPQRSWS0R").privateKey
+    );
+
+    console.log({ depositIdx, recipient, signature });
+
+    await claim({
+      args: [BigInt(depositIdx), recipient, signature],
+    });
+  };
+  const makeDeposit = async () => {
     const seed = await getRandomString(16);
     if (!address) {
       alert("Please connect your wallet");
       return;
     }
+    console.log("seed", { seed });
     // Call the smart contract to make
     await writeContractAsync({
       args: [
@@ -27,13 +66,15 @@ export default function SendPage() {
         false,
         NULL_ADDRESS,
         0,
-        0,
+        false,
+        NULL_ADDRESS,
       ],
+      value: BigInt(10000000000000),
     });
   };
   return (
     <div>
-       <Send/>
+      <Send />
     </div>
   );
 }
--- neo-link-website/lib/utils.ts
+++ neo-link-website/lib/utils.ts
@@ -1,9 +1,14 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { keccak256, toBytes } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
+import { encodePacked, hexToBytes, keccak256, toBytes } from "viem";
+import { privateKeyToAccount, signMessage } from "viem/accounts";
 
 import { nanoid } from "nanoid";
+import {
+  ANYONE_WITHDRAWAL_MODE,
+  NEOLINK_SALT,
+  RECIPIENT_WITHDRAWAL_MODE,
+} from "./constants";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -30,4 +35,43 @@ export function generateKeyFromString(seed: string) {
     address: wallet.address,
     privateKey: privateKey,
   };
+}
+
+export async function signWithdrawalMessage(
+  vaultVersion: string,
+  chainId: string,
+  vaultAddress: `0x${string}`,
+  depositIdx: number,
+  recipient: `0x${string}`,
+  privateKey: `0x${string}`,
+  onlyRecipientMode?: boolean
+) {
+  const extraData = onlyRecipientMode
+    ? RECIPIENT_WITHDRAWAL_MODE
+    : ANYONE_WITHDRAWAL_MODE;
+  const stringHash = keccak256(
+    encodePacked(
+      ["bytes32", "uint256", "address", "uint256", "address", "bytes32"],
+      [
+        NEOLINK_SALT,
+        BigInt(chainId),
+        vaultAddress,
+        BigInt(depositIdx),
+        recipient,
+        extraData,
+      ]
+    )
+  );
+  const stringHashBinary = hexToBytes(stringHash); // Use `toBytes` if it’s not in hex format
+
+  // Create an account instance using the private key
+  const account = privateKeyToAccount(privateKey);
+
+  // Sign the binary hash
+  const signature = await account.signMessage({
+    message: {
+      raw: stringHashBinary,
+    },
+  });
+  return { depositIdx, recipient, signature };
 }
